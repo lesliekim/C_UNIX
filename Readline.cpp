0b0d5df5ad41747efb31b856f0a06be55c92a15e
lesliekim <shulanpan@qq.com>
2015-06-27 20:11:17 +0800

    add realine function

---
+++ Readline.cpp
@@ -0,0 +1,18 @@
+static char* line_read = NULL;
+char* ReadLine()
+{
+	char cache[2000];
+	if(line_read)
+	{
+		free(line_read);
+		line_read = NULL;
+	}
+
+	snprintf(cache, sizeof(cache), "%s@rabbit:%s$ ", getenv("USER"), getcwd(NULL,1000));
+	line_read = readline(cache);
+	if (line_read && *line_read)
+	{
+		add_history(line_read);
+	}
+	return(line_read);
+}
